8fe9a122628dfb9b9cf3aad5807b7ad23d875f2c
James Poeschel <98795839+jamespoeschel@users.noreply.github.com>
2025-12-31 16:07:35 -0500

    Create python.py

---
+++ Toolsets/Export_Tools/Export_AGOL_Attachments/python.py
@@ -0,0 +1,18 @@
+#Contributors: Lan Song, James Poeschel 
+import arcpy
+from arcpy import da
+import os
+
+inTable = arcpy.GetParameterAsText(0)
+fileLocation = arcpy.GetParameterAsText(1)
+
+with da.SearchCursor(inTable, ['DATA', 'ATT_NAME','Object_ID']) as cursor:
+    for item in cursor:
+        attachment = item[0]
+        filenum = str(item[2]) + "_"
+        filename = filenum + str(item[1])
+        open(fileLocation + os.sep + filename, 'wb').write(attachment.tobytes())
+        del item
+        del filenum
+        del filename
+        del attachment
